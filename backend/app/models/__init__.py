55c8b930d5a6ef616766c9e620ed001cb6d39e38
Nour Seddiki <seddikimednour@gmail.com>
2026-03-08 00:04:12 +0100

    fixing db errors

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.event import Event
+from app.models.recommendation import Recommendation
+from app.models.ticket import Ticket
+from app.models.user import User
+
+__all__ = ["User", "Event", "Ticket", "Recommendation"]
